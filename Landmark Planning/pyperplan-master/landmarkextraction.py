b476594c4c56d75d7879dd4c2b942abd1bc4f0ea
Kxrie <jaydenzhang5139@icloud.com>
2021-10-13 07:27:44 +1100

    Added output to file Refactored code to allow for testing multiple approaches Other code cleanup

--- Landmark Planning/pyperplan-master/landmarkextraction.py
+++ Landmark Planning/pyperplan-master/landmarkextraction.py
@@ -1,22 +1,23 @@
 import functools
+import re
+import sys
+from pyperplan import grounding
+from pyperplan.pddl.parser import Parser
 from pyperplan.planner import _parse, _ground
 from pyperplan.search.a_star import astar_search
 from pyperplan.heuristics.landmarks import *
 from pyperplan.heuristics.lm_cut import LmCutHeuristic
 from src.pyperplan.search.a_star import astar_search as astar_search_custom
-from copy import deepcopy
 from pyperplan.heuristics.blind import *
 import os
 
 class ExtractLandmarks():
     '''
-    self.domainfile - location of the domain file
-    self.template - template of task pddl file
-    self.landmarkHeur - List of 
+    self.domainFile - location of the domain file
+    self.taskTemplate - template of task pddl file
 
     self.goals - list of goals
-    self.realgoal - the actual goal
-    self.observations - observations
+    self.realGoalIndex - the actual goal
     self.landmarks - list of landmarks generated from goals
 
     self.debug - whether to print debug comments
@@ -48,14 +49,14 @@ class ExtractLandmarks():
         Loads the necessary resources into class variables. This function is called when
         three arguments are given.
         '''
-        print(f"Getting landmarks for {domaindir} {hypsdir} {templatedir}\n")
-        self.domainfile = os.path.abspath(domaindir)
+        print(f"# Getting landmarks")
+        self.domainFile = os.path.abspath(domaindir)
         with open(hypsdir) as goalsfile:
             self.goals = goalsfile.read().splitlines()
         with open(realhypdir) as realhypfile:
-            self.realgoal = self.goals.index(realhypfile.readline())
+            self.realGoalIndex = self.goals.index(realhypfile.readline())
         with open(templatedir) as templatefile:
-            self.template = templatefile.read()
+            self.taskTemplate = templatefile.read()
 
         # DEBUG
         self.__output(
@@ -64,7 +65,7 @@ class ExtractLandmarks():
         )
         self.__output(
             '# Real Goal parsed:', 
-            f"{self.realgoal} : {self.goals[self.realgoal]}"
+            f"{self.realGoalIndex} : {self.goals[self.realGoalIndex]}"
         )
 
         self.__populate()
@@ -75,52 +76,36 @@ class ExtractLandmarks():
         and uses these task files to extract landmarks.
         '''
         for i in range(len(self.goals)):
-            dirname = self.temp(f"task{i}.pddl")
-            task = self.template.replace("<HYPOTHESIS>", self.goals[i])
+            dirname = self.tempLoc(f"task{i}.pddl")
+            task = self.taskTemplate.replace("<HYPOTHESIS>", self.goals[i])
             with open(dirname, "w") as create:
                 create.write(task)
-            parsed = _parse(self.domainfile, dirname)
-            task = _ground(parsed)
+            parser = Parser(self.domainFile, dirname)
+            dom = parser.parse_domain()
+            problem = parser.parse_problem(dom)
+            task = grounding.ground(problem)
             landmarks = get_landmarks(task)
-            self.landmarks.append(landmarks)
+            landmarks_set = set(map(self.parse_goal, landmarks))
+            self.landmarks.append(landmarks_set)
             
         self.__output(
             '# List of Landmarks calculated:',
-            *[f"{i} : {self.goals[i]} : {a}\n" for i, a in enumerate(self.landmarks)]
+            *[f"{i} : {self.goals[i]} : {a}" for i, a in enumerate(self.landmarks)]
         )
 
     ############################################
     ### FUNCTIONS INTERACTING WITH LANDMARKS ###
     ############################################
-    def table(self):
-        '''Return a table of the landmark intersection for each pair of goals.
-        '''
-        data = [[a.intersection(b) for a in self.landmarks] for b in self.landmarks]
-        # result = pd.DataFrame(data)
-        self.__output(f"Finding intersection table between all landmarks", result= data)
-        return data
-
-    def intersection(self, *args):
-        ''' Finds the intersections of landmarks between given goals
-        '''
-        result = set.intersection(*[self.landmarks[i] for i in args] if len(args) else self.landmarks)
-        self.__output(f"Finding intersection of landmarks between goals: {*args,}", result=result)
-        return result
-    
-    def reducing(self):
-        ''' Trying something new
-        '''
-        initialTask = _ground(_parse(self.domainfile, self.temp("task0.pddl")))
-        #optimal_plans = self.generate_optimal()
-        def toLandmark(acc, landmark):
+    def testApproaches(self):
+        APPROACHES = [self.approach1, self.approach2, self.approach3]
+        def pathToGoal(acc, goal):
             ''' Given a task and a landmark, calculate the number of steps to achieve this landmark
             and calculate the end state after traversing the path.
             '''
-
             task, steps = acc
-            self.__output(f"\n# Finding path to {landmark}")
+            print(f"# Finding path to {goal}")
 
-            task.goals = frozenset({landmark})
+            task.goals = goal
             heuristic = LandmarkHeuristic(task)
             actual = astar_search_custom(task, heuristic, return_state=True)  # Patrick's edited code
             path = astar_search(task, heuristic)  # Generate a path
@@ -136,21 +121,47 @@ class ExtractLandmarks():
             #print(f"Distance from goal {3}: {cost_dif(initialTask, 3, task)}")
             return (task, steps)
 
-        landmarkIntersection =  [i.intersection(self.landmarks[self.realgoal]) for i in self.landmarks]
-        landmarkIntersection[self.realgoal] = {} # Intersection with self to empty set
+        for approach in APPROACHES:
+            self.__output(f"##### Approach: {approach} #####")
+            parser = Parser(self.domainFile, self.tempLoc("task99.pddl"))
+            dom = parser.parse_domain()
+            problem = parser.parse_problem(dom)
+            initialTask = grounding.ground(problem)
+
+            orderedPath = approach(initialTask)
+            task, steps = functools.reduce(pathToGoal, orderedPath, (initialTask, 0))
+            calc = self.parse_goal(self.goals[self.realGoalIndex]) 
+            assert calc.issubset(task.initial_state)  # check that the goal is indeed reached
+            print(f"FINAL RESULT: {steps} steps taken to reach final goal.")
+
+    def approach1(self, initialTask):
+        ''' 
+        Method for picking landmarks:
+            - The goal with the most landmarks in common with the real goal is the most in common.
+            
+        Method for ordering landmarks:
+            - This goal's landmarks are ordered based on similiarity to the initial state.
+        ''' 
+        #optimal_plans = self.generate_optimal()
+        def ordering_score(landmark):
+            ''' Order landmarks based on similiarity to the initial task '''
+            initialTask.goals = landmark
+            landmarks = get_landmarks(initialTask)
+            h = landmarks - initialTask.initial_state
+            print(f"Landmark: {landmark}, Score: {len(h)}")
+            return len(h)
+
+        # PICKING LANDMARKS
+        landmarkIntersection =  [i.intersection(self.landmarks[self.realGoalIndex]) for i in self.landmarks] 
+        landmarkIntersection[self.realGoalIndex] = {} # Intersection with self to empty set
         self.__output(
             "# Intersection of goals with the real goal",
-            *[f"{i}: {a} " if i != self.realgoal else "" for i, a in enumerate(landmarkIntersection)])
+            *[f"{i}: {a} " if i != self.realGoalIndex else "" for i, a in enumerate(landmarkIntersection)])
 
-        # How do I use these to find a deceptive path??
-        # Attempt: the more landmarks there are in common, the more similar the goals are?
         landmarkSet = max(landmarkIntersection, key=len) # Result has a list of landmarks
         self.__output(
             "# The intersection with the largest number of landmarks",
             *[f"{i}: {a} " for i, a in enumerate(landmarkSet)])
-        # TODO: Find a way to order landmarks to reduce path length / cost
-
-
 
         def cost_dif(goal, state_task):
             '''
@@ -178,21 +189,45 @@ class ExtractLandmarks():
 
             return len(state_plan) - optimal_plans[goal]
 
-        def calculate_score(task, landmark):
-            task.goals = frozenset({landmark})
-            test = LandmarkHeuristic(task)
-            h = test.landmarks - test.task.initial_state
-            print(f"Landmark: {landmark}, Score: {len(h)}")
-            return len(h)
-        
-        sorted_h = sorted(landmarkSet, key=lambda landmark: calculate_score(initialTask, landmark))
-        print(f"Sorted based on score: {sorted_h}")
-        return functools.reduce(toLandmark, sorted_h, (initialTask, 0))
+        # LANDMARK ORDERING
+        print(f"# Sorting based on score")
+        ordered_l = sorted(landmarkSet, key=lambda landmark: ordering_score(landmark))
+        self.__output(f"Sorted based on score: {ordered_l}")
+        ordered_l.append(self.parse_goal(self.goals[self.realGoalIndex]))
+        return ordered_l
+
+    def approach2(self, initialTask):
+        ''' 
+        Simply a path to the end.
+        ''' 
+        ordered_l = []
+        ordered_l.append(self.parse_goal(self.goals[self.realGoalIndex]))
+        return ordered_l
+
+    def approach3(self, initialTask):
+        '''
+        Method for picking landmarks:
+            - The goal with the most landmarks in common with the real goal is the most in common.
+        '''
+        landmarkIntersection =  [i.intersection(self.landmarks[self.realGoalIndex]) for i in self.landmarks] 
+        landmarkIntersection[self.realGoalIndex] = {} # Intersection with self to empty set
+        self.__output(
+            "# Intersection of goals with the real goal",
+            *[f"{i}: {a} " if i != self.realGoalIndex else "" for i, a in enumerate(landmarkIntersection)])
+
+        landmarkSetIndex = landmarkIntersection.index(max(landmarkIntersection, key=len)) # Result has a list of landmarks
+        self.__output(
+            "# The index of the goal with the largest number of landmarks in common",
+            landmarkSetIndex)
+        ordered_l = []
+        ordered_l.append(self.parse_goal(self.goals[landmarkSetIndex]))
+        ordered_l.append(self.parse_goal(self.goals[self.realGoalIndex]))
+        return ordered_l
+
 
     ########################
     ### USEFUL FUNCTIONS ###
     ########################
-
     def __output(self, *lines, result=None):
         ''' Function to make pretty outputs.
         '''
@@ -201,41 +236,46 @@ class ExtractLandmarks():
                 print(l)
             if result:
                 print(f"Result: {result}")
-            print("-----------------\n")
+            print("-----------------")
 
-    def temp(self, name):
-        ''' Returns an absolute directory to the
+    def tempLoc(self, name):
+        ''' Returns an absolute directory to the temp location.
         '''
         return os.path.join(self.TEMP_DIR, name)
 
-    def setDebug(self, debugMode = True):
+    def outputLoc(self, name):
+        ''' Returns an absolute directory to the output location.
         '''
-        Whether to have outputs.
+        return os.path.join(self.OUTPUT_DIR, name)
+
+    def setDebug(self, debugMode = True):
+        ''' Whether to have outputs.
         '''
         self.debug = debugMode
 
+    def parse_goal(self, goal):
+        parsedgoals = re.findall('\([A-Za-z0-9 ]*\)', goal)
+        return frozenset(parsedgoals)   
+        '''
+        if len(goal.split('(')) >= 2:
+            goals = goal.split('(')
+            goals = goals[2:]
+            goals = ["(" + g[:-1] for g in goals]
+            return frozenset(goals)
+        else:
+            return frozenset({goal})
+        '''
     def generate_optimal(self):
         optimal_paths = []
-        goal_task = _ground(_parse(self.domainfile, self.temp("task0.pddl")))
+        goal_task = _ground(_parse(self.domainFile, self.tempLoc("task0.pddl")))
         for goal in self.goals:
-            if len(goal.split('(')) >= 2:
-                goals = goal.split('(')
-                goals = goals[2:]
-                goals = ["(" + g[:-1] for g in goals]
-                goal_set = frozenset(goals)
-            else:
-                goal_set = frozenset({goal})
+            goal_set = self.parse_goal(goal)
             goal_task.goals = goal_set
             heuristic = LandmarkHeuristic(goal_task)
             goal_plan = astar_search(goal_task, heuristic)
             optimal_paths.append(len(goal_plan))
             print(f"Calculated length: {len(goal_plan)}")
-
         return optimal_paths
-
-
-
-
 
     ##################################################
     ### UNUSED STUFF WHICH MIGHT BE HANDY LATER ON ###
@@ -253,8 +293,24 @@ class ExtractLandmarks():
             self.goals = tar.extractfile('hyps.dat').read().decode('utf-8').splitlines()
             self.template = tar.extractfile('template.pddl').read().decode('utf-8')
         self.__populate()
-    """
+    
+    def intersection(self, *args):
+        ''' Finds the intersections of landmarks between given goals
+        '''
+        result = set.intersection(*[self.landmarks[i] for i in args] if len(args) else self.landmarks)
+        self.__output(f"Finding intersection of landmarks between goals: {*args,}", result=result)
+        return result
 
+    """
+    """
+    def table(self):
+        '''Return a table of the landmark intersection for each pair of goals.
+        '''
+        data = [[a.intersection(b) for a in self.landmarks] for b in self.landmarks]
+        # result = pd.DataFrame(data)
+        self.__output(f"Finding intersection table between all landmarks", result= data)
+        return data
+    """
     """ UNUSED
     def getLandmark(self, landmark = None):
         '''
@@ -278,18 +334,18 @@ if __name__ == "__main__":
     EXPERIMENTS_TAR_DIR = os.path.join(DIR, 'experiments/tar')
     RESULTS_DIR = os.path.join(DIR, 'results')
     TEMP_DIR = os.path.join(DIR, 'temp')
+    OUTPUT_DIR = os.path.join(DIR, "output") # Location of output folder
 
     # Iterate through each problem set
     for _, dirs, _ in os.walk(EXPERIMENTS_DIR):
         for dname in dirs:
-            print("dname")
             domaindir = f"{EXPERIMENTS_DIR}/{dname}/domain.pddl"
             hypsdir = f"{EXPERIMENTS_DIR}/{dname}/hyps.dat"
             realhypdir = f"{EXPERIMENTS_DIR}/{dname}/real_hyp.dat"
             templatedir = f"{EXPERIMENTS_DIR}/{dname}/template.pddl"
-            
+            sys.stdout = open(os.path.join(OUTPUT_DIR, f"{dname}result.txt"), 'w+')
             extraction = ExtractLandmarks(domaindir, hypsdir, realhypdir, templatedir, debug=True)
-            landmarkslst = extraction.reducing()
+            landmarkslst = extraction.testApproaches()
 
     # For TAR files
     '''
@@ -297,5 +353,3 @@ if __name__ == "__main__":
         for tarfname in files:
             extraction = ExtractLandmarks(f"{EXPERIMENTS_TAR_DIR}/{tarfname}")
     '''
-
-
